890b98355a7ac4bb259f0f0545e06fff42558fa8
Delvin Defoe <defoe@rose-hulman.edu>
2018-09-18 13:07:37 -0400

    jsFunctions posted

---
+++ jsFunctions/js/functions.js
@@ -0,0 +1,7 @@
+// function that returns a single value
+function getRectangleArea(width, height) {
+    return  width * height;
+}
+
+const firstRectangleArea = getRectangleArea(4, 5);
+const secondRectangleArea = getRectangleArea(20, 6);
