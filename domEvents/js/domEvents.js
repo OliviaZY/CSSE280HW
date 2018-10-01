f45b188812492e91c0abb4f6a42ee050fb0e3b1e
Delvin Defoe <defoe@rose-hulman.edu>
2018-10-01 15:49:33 -0400

    domEvents posted

---
+++ domEvents/js/domEvents.js
@@ -0,0 +1,20 @@
+const instructions = ["Say Hello", "Click Me"];
+
+const overMeEl = document.getElementById("overme");
+
+function sayHello() {
+    const helloEl = document.getElementById("demo");
+    // complete this function.
+}
+
+function dispalyDate() {
+    // complete this function.
+}
+
+function passOverMe() {
+    // use this.style to help you solve this
+}
+
+function leaveMe() {
+    // complete this function
+}
