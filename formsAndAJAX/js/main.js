c5a982455a559aaac3b89240c2423312ebd04dcc
Yuan zhou <zhouy4@rose-hulman.edu>
2018-10-04 20:40:08 -0400

    fg

--- formsAndAJAX/js/main.js
+++ formsAndAJAX/js/main.js
@@ -4,9 +4,36 @@
     const apiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=89b61c03';
 
     let responseDiv;
+    let plot, year, title, responseType;
+
+    function getFormData(){
+        plot = $("[name='plot' option:selected").text();
+        responseType=$("[name='r'] option:selected").text();
+        title=$('#t').val();
+        year=$('#y').val();
+        console.log(plot);
+        console.log(responseType);
+        console.log(title);
+        console.log(year);
+        loadMovieData();
+    }
+
+    function loadMovieData() {
+        const inputData = {
+            t: title,
+            r: responseType,
+            y: year,
+            plot: plot
+        };
+         $.ajax(apiUrl, {
+             type: 'GET',
+             data: inputData,
+         });
+    }
 
     $(document).ready(function () {
         //load in initial state
+        const searchByTitle = $('#search-by-title-button').on('click', getFormData);
         responseDiv = $('#search-by-title-response');
     });
 })();
